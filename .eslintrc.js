86ffdeb7cd9f046b8cd8d2e259f4ef6320aaed81
Thomas von Deyen <thomas@vondeyen.com>
2020-11-06 10:30:14 +0100

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/recommended",
+    "plugin:jest/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["vue", "jest"],
+  rules: {},
+}
